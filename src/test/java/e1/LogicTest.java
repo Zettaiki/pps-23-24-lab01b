292d5ebb304bc491f12af0ee0988cd11c1ef9003
Pablo Vargas <psebastian2001@hotmail.com>
2024-03-04 11:41:50 +0100

    Create LogicTest and modified LogicsImpl

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,38 @@
 package e1;
 import org.junit.jupiter.api.*;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  final static int SIZE = 5;
+  Logics logics;
+  Pair<Integer, Integer> pawnPos;
+  Pair<Integer, Integer> knightPos;
+
+  @BeforeEach
+  public void beforeEach() {
+    this.pawnPos = new Pair<>(0,0);
+    this.knightPos = new Pair<>(0, 2);
+    this.logics = new LogicsImpl(SIZE, pawnPos, knightPos);
   }
+
+  @Test
+  public void checkPosition() {
+    assertTrue(this.logics.hasPawn(0, 0));
+    assertFalse(this.logics.hasPawn(0, 2));
+
+    assertTrue(this.logics.hasKnight(0, 2));
+    assertFalse(this.logics.hasKnight(0, 0));
+  }
+
+  @Test
+  public void knightHit() {
+    assertThrowsExactly(IndexOutOfBoundsException.class, () -> {this.logics.hit(100, 100);});
+    assertFalse(this.logics.hit(4, 4));
+    assertFalse(this.logics.hit(2, 1));
+    assertTrue(this.logics.hit(0, 0));
+  }
+
 }
